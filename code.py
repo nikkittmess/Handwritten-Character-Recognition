69e8265747d251c1185f1a859b32a2723a47ca8c
THE MESS <nikkittmessi@gmail.com>
2025-07-05 23:34:37 +0900

    code

--- code.py
+++ code.py
@@ -1,1 +1,12 @@
-Code
+import matplotlib.pyplot as plt
+import cv2
+import numpy as np
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Dropout
+from tensorflow.keras.optimizers import SGD, Adam
+from tensorflow.keras.callbacks import ReduceLROnPlateau, EarlyStopping
+from tensorflow.keras.utils import to_categorical
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.utils import shuffle
